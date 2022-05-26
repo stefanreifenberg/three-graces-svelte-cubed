84ce541c4e749a3fd3a0e5562778c3b44574e43a
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-05-26 11:28:32 +0200

    use mesh instead of primitive + tweak lights

--- src/components/Graces.svelte
+++ src/components/Graces.svelte
@@ -25,14 +25,15 @@
     
     onMount(() => {
         loader.load("models/gltf/graces-draco2.glb", (gltf) => {
-            model = gltf.scene;
+            console.log(gltf)
+            model = gltf.scene.children[0].geometry;
         });
     });
 
     SC.onFrame(() => {
     // follow mouse with light
-    spot.position.x = lerp((spot.position.x -0.5), mouse.x / 200, 0.1)
-    spot.position.y = lerp((spot.position.y), -mouse.y / 200, 0.1)
+    spot.position.x = lerp((spot.position.x -0.5), mouse.x / 250, 0.1)
+    spot.position.y = lerp((spot.position.y), -mouse.y / 250, 0.1)
 
     });
 </script>
@@ -45,21 +46,26 @@
         shadows={THREE.PCFShadowMap}
 >
 	{#if model}
-		 <SC.Primitive
-			 object={model}
-			 scale={[1,1,1]}
-			 position={[0, -2.5, 0]}
-             rotation={[0, -0.2, 0]}
-             castShadow
-             receiveShadow
-		 />
+         <SC.Mesh 
+            geometry={model}
+            material={new THREE.MeshStandardMaterial({
+                color: '#474747',
+                roughness: 0.9,
+                metalness: 0.5
+            })}
+            scale={[1,1,1]}
+            position={[0, -2.5, 0]}
+            rotation={[0, -0.2, 0]}
+            castShadow
+            receiveShadow
+        />
 	{/if}
 
 	<SC.PerspectiveCamera  position={[0, 0, 6]} fov={50} near={3} far={15}/>
 	<SC.DirectionalLight color={0x435c72} intensity={0.08} position={[-100,0,-100]} />
 	<SC.PointLight intensity={1} position={[0, 10, -10]}/>
     <SC.SpotLight
-            intensity={2}
+            intensity={8}
             position={spot.position.toArray()}
             penumbra={2}
             distance={10}
